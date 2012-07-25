584ea2a1306ced9ae25b4ec27d8404e41776415b
Daniel Kabel <daniel.kabel@me.com>
2012-07-25 12:40:53 +0200

    Show RSS Icon in non root accounts

--- templates/show_themen.php
+++ templates/show_themen.php
@@ -82,10 +82,10 @@
                 <a href="<?= URLHelper::getLink($link_delete, array('thema_id' => $result['thema']->getThemaId())) ?>">
                     <?= Assets::img('icons/16/blue/trash.png', array('class' => 'text-top', 'title' => _('Thema inkl. aller Anzeigen löschen'))) ?>
                 </a>
+        <? endif ?>
                 <a href="<?= URLHelper::getLink($link_rss, array('thema_id' => $result['thema']->getThemaId())) ?>">
                     <?= Assets::img('icons/16/blue/rss.png', array('class' => 'text-top', 'title' => _('RSS Feed'))) ?>
                 </a>
-        <? endif ?>
         </div>
         <div style="clear:both; border-bottom: 1px solid #8e8e8e;"></div>
         <div id="list_<?=$result['thema']->getThemaId() ?>" style="display: none;">
