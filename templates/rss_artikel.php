a2f20b7e3848a98efec575d2ed7f5b2955af5e61
Daniel Kabel <daniel.kabel@me.com>
2012-08-27 21:23:14 +0200

    Removed CDATA in RSS items

--- templates/rss_artikel.php
+++ templates/rss_artikel.php
@@ -2,9 +2,9 @@
 echo '<item>
     <title>'.htmlspecialchars(studip_utf8encode($title)).'</title>
     <link>'.htmlspecialchars(studip_utf8encode($studipUrl)).'</link>
-    <description>
-        <![CDATA['.studip_utf8encode(formatready($description,1,1)).']]>
-    </description>
+    <description>'.
+        htmlspecialchars(studip_utf8encode(formatready($description,1,1)))
+    .'</description>
     <pubDate>'.date('r', $pubDate).'</pubDate>
     <guid isPermaLink="false">'.$guid.'</guid>
 </item>
